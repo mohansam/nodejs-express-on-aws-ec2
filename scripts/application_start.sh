95e1cc242e60d559e0f851f27c974aa7274ca50f
mohansam <52488729+mohansam@users.noreply.github.com>
2022-03-18 15:41:42 +0530

    removed timeout

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#create our working directory if it doesnt exist
+DIR="/home/ec2-user/express-app"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
+
+
 #give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ec2-user/express-app
 
@@ -12,7 +22,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
-npm install
-
-#start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null & 
+python test.py
